1da440821bc949f4bf7ef93267289dcdc0500b60
JO_Miraidon <oliveros.jp@obmontessori.edu.ph>
2025-09-19 06:57:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def compute_area(e):
    b1 = float(document.getElementById('num1').value)
    b2 = float(document.getElementById('num2').value)
    height = float(document.getElementById('num3').value)
-   area = (b1 + b2) * h / 2
+   area = (b1 + b2) * height / 2
 
 
    display(f'The area of a trapezoid is {area:.2f}')
+
